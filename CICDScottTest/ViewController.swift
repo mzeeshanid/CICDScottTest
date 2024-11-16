a162cf34eb7863c120e526098c4fefba43fa3ab9
Muhammad Zeeshan <mzeeshanid@yahoo.com>
2024-11-16 16:11:01 +0500

    Initial Commit

---
+++ CICDScottTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CICDScottTest
+//
+//  Created by Ali Aslam on 16/11/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ CICDScottTestTests/CICDScottTestTests.swift
@@ -0,0 +1,17 @@
+//
+//  CICDScottTestTests.swift
+//  CICDScottTestTests
+//
+//  Created by Ali Aslam on 16/11/2024.
+//
+
+import Testing
+@testable import CICDScottTest
+
+struct CICDScottTestTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
